9703684ef01006d9b6a908b31f6b82d758239ad6
Ramadan <daudalaramadan2@gmail.com>
2024-04-07 01:36:51 +0000

    staggerChildren

--- framer-motion-lesson-2/pizzajoint/src/components/Order.js
+++ framer-motion-lesson-2/pizzajoint/src/components/Order.js
@@ -9,11 +9,35 @@ const containerVariants = {
   visible: {
     opacity: 1,
     x: 0,
-    transition: { type: "spring", delay: 0.5 },
+    // ONN LINE 13 IS WHERE WE WANT TO MAKE THE CHILDREN ELEMENT TO DELAY AND DISPLAY ITSELF
+    // AFTER THE PARENT ELEMENTS ANIMATION AHS FULLY BEEN COMPLETED
+    // NOTE THAT WE USE THE WHEN:{} PROPERTY AND THE ARGUMENT IS THE beforeChildren
+    // we could also have the afterChildren which in this case displays the div before the transition staerts
+    // note that the children properties are assiggned to the div that we want it to display after the parent transition has been completed
+    transition: {
+      type: "spring",
+      // these can only be used with the spring
+      // with a heigher mass it means it moves a bit slower and lower mass it means faster
+      //u can try zero u will see the osolating is mooving faster from left to right
+      mass: 0.2,
+      // we use the damping force to make the dumping force a bit bigger
+      //  to stop the opolation
+      damping: 6,
+      when: "beforeChildren",
+
+      // by adding the staggerChildren it slows down the time it displays thechildren
+      // after selecting your items
+      staggerChildren: ".4",
+    },
   },
-  exit: {
-    x: "-100vh",
-    transition: { ease: "easeInOut" },
+};
+
+const childrenVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
   },
 };
 const Order = ({ pizza }) => {
@@ -25,10 +49,14 @@ const Order = ({ pizza }) => {
       animate="visible"
     >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
-      ))}
+      <motion.p variants={childrenVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childrenVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
     </motion.div>
   );
 };
